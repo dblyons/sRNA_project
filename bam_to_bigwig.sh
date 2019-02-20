4073da91f3edfc03291832689b055365871937e1
David Lyons <dlyons@berkeley.edu>
2019-02-20 12:30:50 -0800

    Update bam_to_bigwig.sh

--- bam_to_bigwig.sh
+++ bam_to_bigwig.sh
@@ -6,7 +6,7 @@ echo
 start=$SECONDS
 
 
-bedtools genomecov -bg -scale $1  -ibam $2 | perl -wnl -e '/^\d/ and print;' | perl -wpl -e 's/^/Chr/g;' > tmp1.bg
+bedtools genomecov -bg -scale $1  -ibam $2  perl -wnl -e '/Chr\d/ and print;' > tmp1.bg
 
 echo
 echo 'converting to bigwig'
